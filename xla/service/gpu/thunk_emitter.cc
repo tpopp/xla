2ff60cbb33c4e7506f8c8833f415bdef06dff765
Eugene Zhulenev <ezhulenev@openxla.org>
2026-04-13 15:03:13 -0700

    PR #40751: [xla:gpu] Delete special case for calls to command buffers
    Imported from GitHub PR https://github.com/openxla/xla/pull/40751
    
    This is leftovers from the era when calls were ONLY command buffers. Today we use a command buffer pass that uses thunks, and calls can be anything!
    Copybara import of the project:
    
    --
    cdbd88cd0c2085ebc8465ce6633cfd52b626d849 by Eugene Zhulenev <ezhulenev@openxla.org>:
    
    [xla:gpu] Delete special case for calls to command buffers
    
    Merging this change closes #40751
    
    COPYBARA_INTEGRATE_REVIEW=https://github.com/openxla/xla/pull/40751 from ezhulenev:emit-calls-as-thunks cdbd88cd0c2085ebc8465ce6633cfd52b626d849
    PiperOrigin-RevId: 899200577

--- xla/service/gpu/thunk_emitter.cc
+++ xla/service/gpu/thunk_emitter.cc
@@ -387,64 +387,6 @@ absl::StatusOr<ThunkSequence> ThunkEmitter::EmitSliceToDynamic(
   return thunk_sequence;
 }
 
-AsyncThunkSequence ThunkEmitter::EmitCommandBufferThunk(
-    const HloInstruction* instr) {
-  // Spawn a new ThunkEmitter to emit thunks for the command buffer computation.
-  // Then convert emitted thunks to a sequence of Commands. The resulting thunk
-  // added to the thunk sequence is a CommandBufferThunk. Thunks emitted from
-  // the command buffer computation are discarded.
-  DCHECK_EQ(instr->called_computations().size(), 1);
-  const HloComputation* command_buffer = instr->called_computations().front();
-
-  Thunk::ThunkInfo thunk_info = Thunk::ThunkInfo::WithProfileAnnotation(
-      instr, ir_emitter_context_->GetNextThunkId());
-  return EmitHloComputation(command_buffer)
-      .Map([thunk_info = std::move(thunk_info),
-            &debug_options = ir_emitter_context_->debug_options()](
-               ThunkSequence thunk_sequence) -> absl::StatusOr<ThunkSequence> {
-        // Maybe serialize all commands in a sequence by forcing barriers
-        // between all recorded commands. This guarantees that we execute
-        // all device operations in the exact same order as a thunk
-        // sequence.
-        CommandExecutor::SynchronizationMode synchronization_mode;
-        auto mode = debug_options.xla_gpu_command_buffer_scheduling_mode();
-        switch (mode) {
-          case DebugOptions::SERIALIZE:
-            synchronization_mode =
-                CommandExecutor::SynchronizationMode::kSerialize;
-            break;
-          case DebugOptions::CONCURRENT:
-            synchronization_mode =
-                CommandExecutor::SynchronizationMode::kConcurrent;
-            break;
-          case DebugOptions::LHS:
-            synchronization_mode = CommandExecutor::SynchronizationMode::kLHS;
-            break;
-          default:
-            return Internal("Unsupported command buffer scheduling mode: %d",
-                            mode);
-        }
-
-        bool enable_loop_unroll =
-            debug_options.xla_gpu_command_buffer_unroll_loops();
-        DebugOptions::CommandBufferUpdateMode update_mode =
-            debug_options.xla_gpu_command_buffer_update_mode();
-        ASSIGN_OR_RETURN(
-            CommandExecutor cmd_executor,
-            ConvertToCommands(
-                thunk_sequence,
-                ConvertToCommandsOptions{synchronization_mode,
-                                         enable_loop_unroll, update_mode}));
-
-        return GetThunkSequence(std::make_unique<CommandBufferThunk>(
-            std::move(cmd_executor), std::move(thunk_info),
-            std::make_unique<SequentialThunk>(Thunk::ThunkInfo{},
-                                              std::move(thunk_sequence)),
-            debug_options.xla_enable_command_buffers_during_profiling(),
-            update_mode));
-      });
-}
-
 absl::StatusOr<ThunkSequence> ThunkEmitter::EmitConvolutionThunk(
     const HloCustomCallInstruction* instr) {
   std::vector<ShapedSlice> operand_slices;
@@ -1472,6 +1414,13 @@ AsyncThunkSequence ThunkEmitter::EmitWhile(const HloInstruction* instr) {
             std::move(info), std::move(pred), std::move(cond_thunks),
             std::move(body_thunks), trip_count));
       });
+}
+
+AsyncThunkSequence ThunkEmitter::EmitCallComputation(
+    const HloInstruction* instr) {
+  DCHECK_EQ(instr->called_computations().size(), 1);
+  const HloComputation* computation = instr->called_computations().front();
+  return EmitHloComputation(computation);
 }
 
 absl::StatusOr<ThunkSequence> ThunkEmitter::EmitRngGetAndUpdateState(
@@ -2758,7 +2707,7 @@ AsyncThunkSequence ThunkEmitter::EmitHloInstruction(const HloInstruction* hlo,
     case HloOpcode::kAsyncStart:
       return EmitAsyncStart(hlo);
     case HloOpcode::kCall:
-      return EmitCommandBufferThunk(hlo);
+      return EmitCallComputation(hlo);
     case HloOpcode::kCollectivePermuteDone:
       return IsNvshmemCollective(hlo) ? EmitNvshmemAsyncDone(hlo)
                                       : EmitCollectiveAsyncDone(hlo);
