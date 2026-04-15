239873ed01a3d1d1bc242727261c148a2df00bd2
Thomas Joerg <tjoerg@google.com>
2026-04-15 14:02:20 -0700

    [XLA:GPU] Simple list scheduler to produce concurrent command buffers from Thunk Sequences.
    PiperOrigin-RevId: 900333491

--- xla/backends/gpu/runtime/command_buffer_cmd_emitter.cc
+++ xla/backends/gpu/runtime/command_buffer_cmd_emitter.cc
@@ -15,9 +15,13 @@ limitations under the License.
 
 #include "xla/backends/gpu/runtime/command_buffer_cmd_emitter.h"
 
+#include <algorithm>
+#include <cstddef>
 #include <cstdint>
+#include <deque>
 #include <memory>
 #include <optional>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -27,14 +31,16 @@ limitations under the License.
 #include "absl/log/log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
+#include "absl/strings/str_join.h"
+#include "absl/types/span.h"
 #include "xla/backends/gpu/runtime/all_gather_thunk.h"
 #include "xla/backends/gpu/runtime/all_reduce_thunk.h"
 #include "xla/backends/gpu/runtime/all_to_all_thunk.h"
 #include "xla/backends/gpu/runtime/async_thunk.h"
 #include "xla/backends/gpu/runtime/collective_broadcast_thunk.h"
 #include "xla/backends/gpu/runtime/collective_permute_thunk.h"
-#include "xla/backends/gpu/runtime/collective_thunk.h"
 #include "xla/backends/gpu/runtime/command.h"
 #include "xla/backends/gpu/runtime/command_buffer_cmd.h"
 #include "xla/backends/gpu/runtime/command_executor.h"
@@ -388,47 +394,268 @@ static absl::Status AppendCommands(ConversionContext& ctx,
   }
 }
 
-static absl::Status AppendCommands(ConversionContext& ctx,
-                                   CommandSequence& cmd_sequence,
-                                   const ThunkSequence& sequence,
-                                   const ConvertToCommandsOptions& options) {
-  absl::flat_hash_map<const Thunk*, int64_t> thunk_to_index;
-  absl::flat_hash_map<int64_t, std::vector<int64_t>>
-      concurrent_region_id_to_thunk_indices;
-  std::vector<int64_t> concurrent_region_ids;
-  for (const std::unique_ptr<Thunk>& thunk : sequence) {
-    RETURN_IF_ERROR(AppendCommands(ctx, cmd_sequence, *thunk, options));
-    int64_t index = cmd_sequence.size() - 1;
-    thunk_to_index[thunk.get()] = index;
-    if (thunk->concurrent_region_id().has_value()) {
-      int64_t concurrent_region_id = thunk->concurrent_region_id().value();
-      concurrent_region_id_to_thunk_indices[concurrent_region_id].push_back(
-          index);
-      if (concurrent_region_ids.empty() ||
-          concurrent_region_id > concurrent_region_ids.back()) {
-        concurrent_region_ids.push_back(concurrent_region_id);
+namespace {
+
+// Simple list scheduler for thunks in a concurrent region. Generates a schedule
+// with two concurrent lanes (kMaxConcurrentOps). That is, at most two thunks
+// are executed concurrently. The schedule may execute thunks out of order to
+// achieve better parallelism.
+class ConcurrentRegionScheduler {
+ public:
+  explicit ConcurrentRegionScheduler(absl::Span<Thunk*> thunks) {
+    InitializeSuccessors(thunks);
+    InitializeInDegrees(thunks);
+    Schedule(thunks);
+  }
+
+  // Returns the scheduled thunks in topological order.
+  std::vector<Thunk*> scheduled_thunks() { return scheduled_thunks_; }
+  // Returns the source nodes of the current region, i.e. the nodes that will
+  // execute first.
+  std::vector<Thunk*> scheduled_source_thunks() {
+    return scheduled_source_thunks_;
+  }
+  // Returns the sink nodes of the current region, i.e. the nodes that will
+  // execute last.
+  std::vector<Thunk*> scheduled_sink_thunks() { return scheduled_sink_thunks_; }
+  // Returns artificial dependencies inserted by the scheduler.
+  absl::flat_hash_map<const Thunk*, std::vector<Thunk*>>
+  scheduled_successors() {
+    return scheduled_successors_;
+  }
+
+ private:
+  // Build map of successors from buffer conflicts among thunks.
+  void InitializeSuccessors(absl::Span<Thunk*> thunks) {
+    // Pre-compute buffer read-write sets for all thunks in this sequence.
+    std::vector<BufferUse::ReadWriteSet> rw_sets(thunks.size());
+    for (size_t i = 0; i < thunks.size(); ++i) {
+      thunks[i]->Walk(
+          [&](auto* thunk) { rw_sets[i].AddAll(thunk->buffer_uses()); });
+    }
+    for (size_t i = 0; i < thunks.size(); ++i) {
+      for (size_t j = i + 1; j < rw_sets.size(); ++j) {
+        if (rw_sets[j].HasConflicts(rw_sets[i])) {
+          thunks_[thunks[i]].successors.push_back(thunks[j]);
+        }
       }
-      CHECK_GE(concurrent_region_id, concurrent_region_ids.back())
-          << "Concurrent region ids are not monotonic.";
+    }
+    // TODO(tjoerg): Thunks support resource dependencies. Though they are
+    // currently not used for XLA GPU, support should be added in the future.
+  }
+
+  // Initialize in-degrees of all thunks, based on the successors map.
+  void InitializeInDegrees(absl::Span<Thunk*> thunks) {
+    for (size_t i = 0; i < thunks.size(); ++i) {
+      for (Thunk* successor : thunks_[thunks[i]].successors) {
+        thunks_[successor].in_degree++;
+      }
+    }
+    if (VLOG_IS_ON(5)) {
+      for (Thunk* thunk : thunks) {
+        VLOG(5) << "In-degree of " << thunk->thunk_info().profile_annotation
+                << ": " << thunks_[thunk].in_degree;
+      }
     }
   }
 
+  // Insert thunks with in-degree 0 into the ready queue. The queue is divided
+  // into kMaxConcurrentOps lanes for cache locality. For initialization, simply
+  // assign to lanes in a round robin fashion.
+  void InitializeReady(absl::Span<Thunk*> thunks) {
+    for (size_t i = 0; i < thunks.size(); ++i) {
+      if (thunks_[thunks[i]].in_degree == 0) {
+        ready_[i % kMaxConcurrentOps].push_back(thunks[i]);
+        ready_size_++;
+      }
+    }
+  }
+
+  void EnqueueReady(Thunk* thunk, size_t lane_id) {
+    // Insert in the front for cache locality.
+    ready_[lane_id].push_front(thunk);
+    ready_size_++;
+  }
+
+  // Move ready thunks to the active list. Prefer scheduling thunks in the same
+  // lane as their data predecessors.
+  void MoveReadyToActive(std::vector<Thunk*>& active) {
+    size_t num_ops_to_move = std::min(kMaxConcurrentOps, ready_size_);
+    for (size_t i = 0; i < num_ops_to_move; ++i) {
+      // Prefer thunks in the same lane, but use thunks from other lanes if
+      // necessary.
+      if (!ready_[i].empty()) {
+        active.push_back(ready_[i].front());
+        ready_[i].pop_front();
+        ready_size_--;
+      } else {
+        for (size_t j = 0; j < kMaxConcurrentOps; ++j) {
+          if (!ready_[j].empty()) {
+            active.push_back(ready_[j].front());
+            ready_[j].pop_front();
+            ready_size_--;
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // Returns the number of thunks in the ready queue.
+  size_t ready_size() const { return ready_size_; }
+
+  // Generate a schedule: While there are thunks in the ready queue, move ready
+  // thunks to the active list, update in-degrees of successors, update the
+  // ready queue, and start over.
+  void Schedule(absl::Span<Thunk*> thunks) {
+    InitializeReady(thunks);
+    std::vector<Thunk*> active;
+    std::vector<Thunk*> previously_active;
+    scheduled_sink_thunks_.resize(kMaxConcurrentOps);
+
+    while (ready_size() > 0) {
+      MoveReadyToActive(active);
+      VLOG(5) << "Active list: "
+              << absl::StrJoin(
+                     active, ", ", [](std::string* out, const Thunk* thunk) {
+                       absl::StrAppend(out,
+                                       thunk->thunk_info().profile_annotation);
+                     });
+      // Record the first active thunks of this region.
+      if (scheduled_source_thunks_.empty()) {
+        scheduled_source_thunks_ = active;
+      }
+      // Update in-degrees of successors and enqueue ready thunks.
+      for (size_t lane_id = 0; lane_id < active.size(); ++lane_id) {
+        Thunk* thunk = active.at(lane_id);
+        for (Thunk* successor : thunks_[thunk].successors) {
+          thunks_[successor].in_degree--;
+          if (thunks_[successor].in_degree == 0) {
+            EnqueueReady(successor, lane_id);
+          }
+        }
+        scheduled_thunks_.push_back(thunk);
+
+        // Add dependencies to previously active ops.
+        if (lane_id < previously_active.size()) {
+          Thunk* previously_active_same_lane = previously_active.at(lane_id);
+          scheduled_successors_[previously_active_same_lane].push_back(thunk);
+        }
+      }
+      previously_active = active;
+      // Record the active thunks as sinks for now. Note that last while loop
+      // iteration may not have all lanes filled, hence do it on each iteration.
+      for (size_t lane_id = 0; lane_id < active.size(); ++lane_id) {
+        scheduled_sink_thunks_[lane_id] = active[lane_id];
+      }
+      active.clear();
+    }
+  }
+
+  // Number of concurrent lanes in the schedule.
+  // Using 2 lanes is a conservative default. Higher degrees of concurrency are
+  // prone to jitter, i.e. variations in the execution time. When multiple GPUs
+  // run in lockstep, the worst case performance determines the overall speed.
+  static constexpr size_t kMaxConcurrentOps = 2;
+  // Ready queue with kMaxConcurrentOps lanes.
+  std::deque<Thunk*> ready_[kMaxConcurrentOps];
+
+  // See comments on the methods above.
+  size_t ready_size_ = 0;
+  struct ThunkState {
+    std::vector<Thunk*> successors;
+    int64_t in_degree = 0;
+  };
+  absl::flat_hash_map<const Thunk*, ThunkState> thunks_;
+  std::vector<Thunk*> scheduled_thunks_;
+  absl::flat_hash_map<const Thunk*, std::vector<Thunk*>> scheduled_successors_;
+  std::vector<Thunk*> scheduled_source_thunks_;
+  std::vector<Thunk*> scheduled_sink_thunks_;
+};
+
+absl::Status AppendCommandsInConcurrentRegions(
+    ConversionContext& ctx, CommandSequence& cmd_sequence,
+    const ThunkSequence& sequence, const ConvertToCommandsOptions& options) {
+  absl::flat_hash_map<int64_t, std::vector<Thunk*>>
+      concurrent_region_id_to_thunks;
+  std::vector<int64_t> concurrent_region_ids;
+  for (const std::unique_ptr<Thunk>& thunk : sequence) {
+    TF_RET_CHECK(thunk->concurrent_region_id().has_value())
+        << "Concurrent region id must be set in scheduling mode "
+           "kConcurrentRegions.";
+    int64_t concurrent_region_id = thunk->concurrent_region_id().value();
+    concurrent_region_id_to_thunks[concurrent_region_id].push_back(thunk.get());
+    if (concurrent_region_ids.empty() ||
+        concurrent_region_id > concurrent_region_ids.back()) {
+      concurrent_region_ids.push_back(concurrent_region_id);
+    }
+    CHECK_GE(concurrent_region_id, concurrent_region_ids.back())
+        << "Concurrent region ids are not monotonic.";
+    VLOG(3) << "Thunk " << thunk->thunk_info().profile_annotation
+            << " is in region " << concurrent_region_id;
+  }
+  std::vector<ConcurrentRegionScheduler> concurrent_region_schedules;
+  absl::flat_hash_map<const Thunk*, int64_t> thunk_to_index;
+  for (int64_t concurrent_region_id : concurrent_region_ids) {
+    std::vector<Thunk*> thunks =
+        concurrent_region_id_to_thunks[concurrent_region_id];
+    ConcurrentRegionScheduler& scheduler =
+        concurrent_region_schedules.emplace_back(absl::MakeSpan(thunks));
+    for (Thunk* thunk : scheduler.scheduled_thunks()) {
+      TF_RETURN_IF_ERROR(AppendCommands(ctx, cmd_sequence, *thunk, options));
+      int64_t index = cmd_sequence.size() - 1;
+      thunk_to_index[thunk] = index;
+    }
+  }
   // Ensure extra_resources is sized to cover all commands added so far
   // (including those added by nested AppendCommands calls).
   ctx.extra_resources.resize(cmd_sequence.size());
 
-  // Add dependencies between concurrent regions to serialize them.
-  for (int64_t i = 1; i < concurrent_region_ids.size(); ++i) {
-    int64_t concurrent_region_id = concurrent_region_ids[i - 1];
-    int64_t next_concurrent_region_id = concurrent_region_ids[i];
-    for (int64_t thunk_index :
-         concurrent_region_id_to_thunk_indices[concurrent_region_id]) {
-      for (int64_t next_thunk_index :
-           concurrent_region_id_to_thunk_indices[next_concurrent_region_id]) {
-        ctx.extra_resources[thunk_index].push_back(
-            ResourceUse::Read(cmd_sequence[next_thunk_index]->token()));
+  // Add dependencies within concurrent regions.
+  for (auto& scheduler : concurrent_region_schedules) {
+    for (auto& [thunk, successors] : scheduler.scheduled_successors()) {
+      for (Thunk* successor : successors) {
+        ctx.extra_resources[thunk_to_index[thunk]].push_back(ResourceUse::Read(
+            cmd_sequence[thunk_to_index[successor]]->token()));
       }
     }
+  }
+  // Add dependencies between concurrent regions. The source nodes of subsequent
+  // regions depend on all sink nodes of prior regions.
+  for (size_t i = 1; i < concurrent_region_schedules.size(); ++i) {
+    for (Thunk* sink :
+         concurrent_region_schedules[i - 1].scheduled_sink_thunks()) {
+      if (sink == nullptr) {
+        continue;
+      }
+      for (Thunk* source :
+           concurrent_region_schedules[i].scheduled_source_thunks()) {
+        // The source nodes of region `i` depend on the sink nodes of region
+        // `i-1` to ensure serialization between regions.
+        ctx.extra_resources[thunk_to_index[source]].push_back(
+            ResourceUse::Read(cmd_sequence[thunk_to_index[sink]]->token()));
+      };
+    }
+  }
+  return absl::OkStatus();
+}
+
+}  // namespace
+
+static absl::Status AppendCommands(ConversionContext& ctx,
+                                   CommandSequence& cmd_sequence,
+                                   const ThunkSequence& sequence,
+                                   const ConvertToCommandsOptions& options) {
+  if (options.synchronization_mode ==
+      CommandExecutor::SynchronizationMode::kConcurrentRegions) {
+    return AppendCommandsInConcurrentRegions(ctx, cmd_sequence, sequence,
+                                             options);
+  }
+  absl::flat_hash_map<const Thunk*, int64_t> thunk_to_index;
+  for (const std::unique_ptr<Thunk>& thunk : sequence) {
+    RETURN_IF_ERROR(AppendCommands(ctx, cmd_sequence, *thunk, options));
+    int64_t index = cmd_sequence.size() - 1;
+    thunk_to_index[thunk.get()] = index;
   }
 
   return absl::OkStatus();
