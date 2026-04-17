94ebb320cbfe5237c543fc39e154b77760e4bb98
Levon Ter-Grigoryan <patrios@google.com>
2026-04-17 02:54:02 -0700

    Rollback PR #37936: since it breaks ragged_collective_test_gpu in JAX when machine have >2 GPUs.
    Reverts 13760ad1f1df797b5f21828084b168dc652368a0
    
    PiperOrigin-RevId: 901185068

--- xla/backends/gpu/runtime/collective_thunk.cc
+++ xla/backends/gpu/runtime/collective_thunk.cc
@@ -385,16 +385,13 @@ absl::Status CollectiveThunk::ExecuteOnStream(const ExecuteParams& params) {
             debug_options
                 .xla_gpu_first_collective_call_terminate_timeout_seconds()));
   };
-  std::pair<RendezvousFlag*, RendezvousFlag*> rend_flags;
-  ASSIGN_OR_RETURN(
-      rend_flags,
-      params.collective_cliques->GetCliqueFirstRendezvousFlags(clique_key));
-  RETURN_IF_ERROR(first_call_rendezvous("before", *(rend_flags.first)));
+
+  RETURN_IF_ERROR(first_call_rendezvous("before", pre_call_rendezvous_flag_));
 
   // Launch collective operation on the compute stream.
   RETURN_IF_ERROR(RunCollective(params, clique_key, *params.stream, *comm));
 
-  RETURN_IF_ERROR(first_call_rendezvous("after", *(rend_flags.second)));
+  RETURN_IF_ERROR(first_call_rendezvous("after", post_call_rendezvous_flag_));
 
   return absl::OkStatus();
 }
