2fb37df50c0bc0554fc8aec94c53af74586f17b9
Henning Becker <hebecker@google.com>
2026-04-24 02:25:03 -0700

    Reverts 5af7edc354dfbcb2faf7cbc2b71a67f3287cdf52
    PiperOrigin-RevId: 904898476

--- xla/debug_options_flags.cc
+++ xla/debug_options_flags.cc
@@ -510,6 +510,8 @@ DebugOptions DefaultDebugOptionsIgnoringFlags() {
 
   opts.set_xla_gpu_enable_pdl(true);
   opts.set_xla_gpu_command_buffer_update_mode(DebugOptions::ALWAYS_UPDATE);
+
+  opts.set_xla_gpu_experimental_aot_compiled_thunks(true);
   return opts;
 }
 
