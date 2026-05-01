9c69d090b8f6b8cdd561b50f67fc68ea17476ced
Sameer Agarwal <sameeragarwal@google.com>
2026-05-01 09:07:12 -0700

    Reverts 888f2ed3059031b54d01e29766ac8347515e00d6
    PiperOrigin-RevId: 908761448

--- build_tools/ci/build.py
+++ build_tools/ci/build.py
@@ -394,6 +394,8 @@ Build(
         "-//xla/backends/cpu/collectives:gloo_collectives_test",
         "-//xla/backends/cpu/collectives:mpi_collectives",
         "-//xla/backends/cpu/collectives:mpi_communicator",
+        # ortools is not windows compatible
+        "-//xla/hlo/experimental/...",
         # implementation is not windows compatible
         "-//xla/python/transfer/...",
         "-//xla/backends/profiler/subprocess:subprocess_profiling_session",
@@ -691,6 +693,7 @@ Build(
     configs=("nonccl",),
     target_patterns=(
         "//xla/...",
+        "-//xla/hlo/experimental/...",
         "-//xla/python_api/...",
         "-//xla/python/...",
         "-//xla/service/gpu/...",
@@ -724,6 +727,7 @@ Build(
     configs=("nonccl",),
     target_patterns=(
         "//xla/...",
+        "-//xla/hlo/experimental/...",
         "-//xla/python_api/...",
         "-//xla/python/...",
         "-//xla/service/gpu/...",
